c4886acf2402f2b42947ec2a205a6e5a4ba94ce8
jose <tonyjimenez099.tj@gmail.com>
2022-06-29 15:04:50 -0500

    commit tree 73cc9d37d2637955118369ca6a6ef00c966f0b30

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+    let multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random());
+
+let num3 = 29;
+let num4 = 5;
+    const mod = num3 % num4;
+
+    const max = Math.max(1, 2, 3, 4, 5, 20);
